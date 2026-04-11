aefbe50c4b0ebeb35c7a06803af8e4885d2de02d
Prem Mourya <premmourya084@gmail.com>
2026-04-11 14:51:36 +0530

    api updated

--- src/Service/service.js
+++ src/Service/service.js
@@ -6,7 +6,7 @@ import { ADMIN_DETAILS, USER_DETAILS } from "../Constant/Constant";
 // export const BASE_URL = "http://localhost:3000/";
 
 export const COMMON_IMAGE_URL = "/api";
-export const BASE_URL = "/";
+export const BASE_URL = "/api";
 const createApiForClient = (contentType) => {
   const headers = { "Cache-Control": "no-cache" };
 
